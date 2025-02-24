1b1410b88c2ec0a7bed2bd784359b4038e00e4d2
Alexandru Hohota <alex.hohota@gmail.com>
2025-02-24 10:59:49 +0200

    +

--- next.config.js
+++ next.config.js
@@ -36,6 +36,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "proshelves-app-m75ot.ondigitalocean.app",
+      },
     ],
   },
 }
